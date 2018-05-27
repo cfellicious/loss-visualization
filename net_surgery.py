4abbf0ff0d56acc0ce95f7daa297d035b289e916
chris <cfellicious@yahoo.co.in>
2018-05-27 16:31:09 +0200

    Added function to save the initial values of the code. Specified the start and end layers of the net forward function

--- net_surgery.py
+++ net_surgery.py
@@ -1,4 +1,5 @@
 import caffe
+import copy
 import random
 import numpy as np
 import time
@@ -27,11 +28,13 @@ def calculate_param_count(net=None):
             # TODO: Add bias values too. Currently, only weights are used.
             curr_layer_shape = net.params[layer][0].data.shape
             norm = norm + (net.params[layer][0].data * net.params[layer][0].data).sum()
+            norm = norm + (net.params[layer][1].data * net.params[layer][1].data).sum()
             layer_params = 1
             for layer_shape in curr_layer_shape:
                 layer_params = layer_params * layer_shape
 
-            param_count = param_count + layer_params
+            bias_params = net.params[layer][1].data.shape
+            param_count = param_count + layer_params + bias_params[0]
 
     norm = np.sqrt(norm)
     return param_count, norm
@@ -91,21 +94,64 @@ def create_grid(vectors=None, steps=0):
     return value_matrix
 
 
-def update_net_params(net, vector1, vector2):
+def update_net_params(net, layer_weights, vector1, vector2):
+    """
+    This function updates the network weights obtained from the initial weights
+    :param net: The CAFFE network
+    :param layer_weights: The saved weights
+    :param vector1: The vector whose weights are to be added to the network
+    :param vector2: THe vector whose weights are to be added to the network
+    :return:
+    """
     layer_names = net.blobs
+
+    # iterate through all the layers
     for layer in layer_names:
         curr_layer = net.layer_dict.get(layer, None)
         if curr_layer is None:
             continue
 
         vec_idx = 0
+        # Update only convolutional and fully connected layers
         if net.layer_dict.get(layer, None).type in ('Convolution', 'InnerProduct'):
+
+            # Update the weights of the layer
             layer_length = np.shape(net.params[layer][0].data.flat)[0]
-            net.params[layer][0].data.flat = \
-                net.params[layer][0].data.flat + \
-                vector1[vec_idx:vec_idx + layer_length] + \
-                vector2[vec_idx:vec_idx+layer_length]
+            weights = layer_weights.get(layer).get('weights')
+            net.params[layer][0].data.flat = weights + \
+                                             vector1[vec_idx:vec_idx + layer_length] + \
+                                             vector2[vec_idx:vec_idx+layer_length]
             vec_idx = vec_idx + layer_length
+
+            # Update the biases of the layer
+            bias_values = layer_weights.get(layer).get('bias')
+            bias_length = net.params[layer][1].data.shape[0]
+            net.params[layer][1].data.flat = bias_values  + \
+                                        vector1[vec_idx:vec_idx + bias_length] + \
+                                        vector2[vec_idx:vec_idx + bias_length]
+
+            vec_idx = vec_idx + bias_length
+
+
+    return net
+
+
+def save_network_weights(net=None):
+    if net is None:
+        return None
+
+    layer_names = net.blobs
+    net_weights = dict()
+    for layer in layer_names:
+        curr_layer = net.layer_dict.get(layer, None)
+        if curr_layer is None:
+            continue
+        if net.layer_dict.get(layer, None).type in ('Convolution', 'InnerProduct'):
+            layer_data = {'weights': np.array(net.params[layer][0].data.flat, copy=True),
+                          'bias': np.array(net.params[layer][1].data.flat, copy=True)}
+            net_weights[layer] = copy.deepcopy(layer_data)
+
+    return copy.deepcopy(net_weights)
 
 
 def create_loss_landscape(net=None, vectors=None):
@@ -116,15 +162,29 @@ def create_loss_landscape(net=None, vectors=None):
     :return: A matrix containing the loss values for each point in the grid
     """
     steps = 15
+    debug = 1
     start_time = time.time()
-    vector_grid1 = create_grid(np.vstack((vectors[0], np.negative(vectors[0]))), steps)
-    vector_grid2 = create_grid(np.vstack((vectors[1], np.negative(vectors[1]))), steps)
+    # if not debug, calculate the grid and save the new data
+    if not debug:
+        vector_grid1 = create_grid(np.vstack((vectors[0], np.negative(vectors[0]))), steps)
+        vector_grid2 = create_grid(np.vstack((vectors[1], np.negative(vectors[1]))), steps)
+
+        np.save('./vector_grid1', vector_grid1)
+        np.save('./vector_grid2', vector_grid2)
+    else:
+        vector_grid1 = np.load('./vector_grid1.npy')
+        vector_grid2 = np.load('./vector_grid2.npy')
+
     end_time = time.time() - start_time
     print('Duration : ' + str(end_time))
 
     # Load a default image and set it as the data
     im = caffe.io.load_image('/home/chris/PycharmProjects/loss-visualization/airplane1.png')
     net.blobs['data'] = np.asarray(im)
+
+
+    # Save the initial weights of the network
+    layer_weights = save_network_weights(net=net)
 
     print('Default Loss:', net.forward())
 
@@ -133,9 +193,11 @@ def create_loss_landscape(net=None, vectors=None):
         for y_idx in range(0, steps):
             print(x_idx, y_idx)
             # Modify the network values
-            update_net_params(net, vector_grid1[x_idx, :], vector_grid2[y_idx, :])
+            net = update_net_params(net, layer_weights, vector_grid1[x_idx, :], vector_grid2[y_idx, :])
             # Calculate the loss
-            loss = net.forward()
+            im = caffe.io.load_image('/home/chris/PycharmProjects/loss-visualization/airplane1.png')
+            net.blobs['data'] = np.asarray(im)
+            loss = net.forward(start='conv1', end='loss')
             # Save the loss value to a matrix
             loss_matrix[x_idx][y_idx] = loss.get('loss', 0)
 
@@ -146,7 +208,10 @@ def main():
     # Load the network into memory through the pyCaffe Interface
     # TODO: The solver and prototxt files should be chosen through GUI
     net = caffe.Net('/home/chris/caffe/python/solver.prototxt',
-                    '/home/chris/caffe/python/model.caffemodel', caffe.TRAIN)
+                    '/home/chris/caffe/python/model.caffemodel', caffe.TEST)
+
+    # Set the mode as GPU for CAFFE
+    caffe.set_mode_gpu()
 
     # Calculate the total parameter count in the network
     # Calculate the Frobenius norm/Euclidean norm of the network
@@ -164,6 +229,10 @@ def main():
 
     # Multiply the vectors with the norm of the Network
     directional_vectors = np.multiply(normalized_vectors, euclidean_norm)
+
+    # save the matrix of directional_vectors
+    # save the numpy array
+    np.save('directional_vectors', directional_vectors)
 
     loss_values = create_loss_landscape(net, directional_vectors)
 
